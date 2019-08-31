600555db84f93000b298b486555070942418cf16
Lars Grevelink <lars.grevelink@mobian.global>
2019-08-31 17:31:01 +0200

    Besides the trimming we should also remove new lines from the descriptions

--- src/Helpers/PropertyHelper.php
+++ src/Helpers/PropertyHelper.php
@@ -20,9 +20,10 @@ class PropertyHelper
         $docComment = $property->getDocComment() ?: '';
 
         $trimmed = trim(preg_replace('#[ \t]*(?:\/\*\*|\*\/|\*)?[ \t]{0,1}(.*)?#u', '$1', $docComment));
+        $cleaned = str_replace(PHP_EOL, ' ', $trimmed);
 
         $name = '$' . $property->getName();
-        $description = trim(Str::substr($trimmed, 0, mb_strpos($trimmed, '@')));
+        $description = trim(Str::substr($cleaned, 0, mb_strpos($cleaned, '@') ?: mb_strlen($cleaned)));
 
         preg_match('/\@var ([a-z]+)/', $trimmed, $matches);
 
